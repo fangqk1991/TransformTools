08174b0671f9570fd6ca131fac7e8822d3737343
fangqk1991 <me@fangqk.com>
2018-06-23 00:38:41 +0800

    make-app

---
+++ tools/make-app.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+__FILE__="$0"
+REAL_FILE=`readlink "${__FILE__}"`
+if [ ! -z "${REAL_FILE}" ]; then
+    __FILE__="${REAL_FILE}"
+fi
+
+__DIR__=`cd "$(dirname "${__FILE__}")"; pwd`
+
+buildDir=`cd "${__DIR__}/../build"; pwd`
+
+IFS='
+'
+
+files=`find "${__DIR__}/../modules" -name "*.sh"`
+
+for file in ${files}; do
+
+    filename=`basename "${file%.*}"`
+
+    # For Apple Script
+    # echo do shell script \""${file}"\" > "${__DIR__}/build/${filename}.applescript"
+
+    # For Mac App
+    DIR="${buildDir}/${filename}.app/Contents/MacOS"
+    mkdir -p "$DIR"
+
+    targetFile="${DIR}/${filename}"
+
+    sed '/^$/d' "${file}" \
+    | sed '{
+        1s/echo "$1"/pbpaste/
+        1i\#!/bin/bash
+        $s/^\(.*\)$/\1 | pbcopy/
+    }'> "${targetFile}"
+
+    chmod +x "${targetFile}"
+
+    echo "${buildDir}/${filename}.app"
+done
