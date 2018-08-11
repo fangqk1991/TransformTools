2a2de82ec02b728d9391aafd3595f1d58de1be5f
fangqk1991 <me@fangqk.com>
2018-08-11 10:40:52 +0800

    old modules migrate

--- modules/name-to-small-camel.sh
+++ modules/name-to-small-camel.sh
@@ -1,5 +1,15 @@
-echo "$1" | sed '{
+#!/bin/bash
+
+###### fc_input start ######
+fc_input="$1"
+###### fc_input end ######
+
+fc_output="$(echo "${fc_input}" | sed '{
     :start
     s/[_-]\([a-z]\)/\u\1/
     t start
-}'
+}')"
+
+###### fc_output start ######
+echo "${fc_output}"
+###### fc_output end ######
