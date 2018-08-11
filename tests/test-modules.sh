dc7666f9a631414630ec7b68a8c16a5a5add844e
fangqk1991 <me@fangqk.com>
2018-08-11 10:38:20 +0800

    test scripts

---
+++ tests/test-modules.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+__FILE__="$0"
+REAL_FILE=`readlink "${__FILE__}"`
+if [ ! -z "${REAL_FILE}" ]; then
+    __FILE__="${REAL_FILE}"
+fi
+
+__DIR__=`cd "$(dirname "${__FILE__}")"; pwd`
+
+
+files=`find "${__DIR__}/../modules" -name "*.sh"`
+
+for file in ${files}; do
+
+    filename=`basename "${file%.*}"`
+    datafile="${__DIR__}/${filename}.txt"
+
+    if [ ! -f "${datafile}" ]; then
+        echo "${filename} test fail. The test data missing.."
+    else
+        cat "${datafile}" | "${file}"
+    fi 
+done
